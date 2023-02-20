be8f8cd5a428fcc403b1c18c5e87b2866afd62a2
Code Sandbox <hello@codesandbox.io>
2023-02-20 23:27:37 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f268bc98ba351ceee7f66a32bb3c0b38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a545e795bc01ffbfc18",
+    "url": "https://emanuelcarvalho2004.github.io/csb-ir6qgp/static/css/main.2d7d53c0.chunk.css"
+  },
+  {
+    "revision": "2a545e795bc01ffbfc18",
+    "url": "https://emanuelcarvalho2004.github.io/csb-ir6qgp/static/js/main.2a545e79.chunk.js"
+  },
+  {
+    "revision": "d3158d9747f7637e38c9",
+    "url": "https://emanuelcarvalho2004.github.io/csb-ir6qgp/static/js/1.d3158d97.chunk.js"
+  },
+  {
+    "revision": "2988607c2e96dc8b1432",
+    "url": "https://emanuelcarvalho2004.github.io/csb-ir6qgp/static/js/runtime~main.2988607c.js"
+  },
+  {
+    "revision": "0d03b50174a4a61e69b35bc4d5831bb6",
+    "url": "https://emanuelcarvalho2004.github.io/csb-ir6qgp/index.html"
+  }
+];
